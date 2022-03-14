3691ebc26302f41c449d68d0753f8005a5cf5672
YeongChangYun <94744740+YeongT@users.noreply.github.com>
2022-03-14 12:47:09 +0900

    refactor: jwtSign mechanism and deploy env-setup method (#17)
    * refactor : change jwtSign mechanism
    
    * refactor: change deploy environment method

--- src/auth/emailCheck.ts
+++ src/auth/emailCheck.ts
@@ -1,17 +1,17 @@
 import { Request, Response, Router } from 'express';
 import { DBError } from '../../server/app';
-import respond from '../coms/respondClient';
-import { validators, validatorErrorChecker } from '../coms/validator';
+import { respond } from '../coms/respondClient';
+import { validatorErrorChecker, validators } from '../coms/validator';
 import { ERR_DB, ERR_EMAIL_DUPLICATION } from '../coms/errorMessage';
 
-import User from '../../models/user';
+import UserModel from '../../models/user';
+
 const router = Router();
 
-const emailDuplicationCheck = async (res: Response, email: string) => {
-  const _user = await User.findOne({ 'account.email': `${email}` }, { _id: 0 });
-  if (!(_user === null || _user === undefined))
-    return respond(res, ERR_EMAIL_DUPLICATION.code, ERR_EMAIL_DUPLICATION.msg, null, null);
-  return null;
+const emailDuplicationCheck = async (res: Response, email: string): Promise<boolean> => {
+  const _user = await UserModel.findOne({ 'account.email': `${email}` }, { _id: 0 });
+  if (_user !== null) respond(res, ERR_EMAIL_DUPLICATION.code, ERR_EMAIL_DUPLICATION.msg, null, null);
+  return _user === null;
 };
 
 router.post('/', validators.email, async (req: Request, res: Response) => {
@@ -19,9 +19,9 @@ router.post('/', validators.email, async (req: Request, res: Response) => {
   if (DBError !== null) return respond(res, ERR_DB.code, ERR_DB.msg, null, DBError);
 
   //# CHECK PARAMETER ERROR
-  if (validatorErrorChecker(req, res) !== null) return;
+  if (!validatorErrorChecker(req, res)) return;
 
-  if ((await emailDuplicationCheck(res, req.body.email)) !== null) return;
+  if (!(await emailDuplicationCheck(res, req.body.email))) return;
   else respond(res, 200, 'THIS EMAIL CAN BE USED', null, null);
 });
 
--- src/coms/currentTime.ts
+++ src/coms/currentTime.ts
@@ -1,6 +1,7 @@
-const currentTime = () => {
-  var moment = require('moment');
-  require('moment-timezone');
+import moment from 'moment';
+import 'moment-timezone';
+
+const currentTime = (): string => {
   moment.tz.setDefault('Asia/Seoul');
   return moment().format('YYYY-MM-DD HH:mm:ss.SSS');
 };
