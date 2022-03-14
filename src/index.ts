dc215210163359d0749726bccbd3c9fc3d3f05d5
YeongChangYun <94744740+YeongT@users.noreply.github.com>
2022-03-14 23:01:38 +0900

    feat : add status check page & setup xml (#19)
    * feat : add status check page & setup xml
    
    * fix : solve misconfig in xml

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { Request, Response, Router } from 'express';
 import { DBError } from '../server/app';
 import { emailCheckAPI } from './auth/emailCheck';
 import registerAPI from './auth/register';
+import { configErrors } from './coms/errorMessage';
 
 const router = Router();
+
+interface statusCheck {
+  name: string;
+  error: string | null;
+}
 
 router.get('/', (req: Request, res: Response) => {
   const DB_STATUS =
@@ -15,6 +21,29 @@ router.get('/', (req: Request, res: Response) => {
   res.write('<link rel="stylesheet" href="https://www.loadover.me/common/styles/apiMain.css">');
   res.write('Welcome!<br>This is API Server of Local-Community<br><br>');
   res.end(`Database Connection : ${DB_STATUS}`);
+});
+
+router.get('/status', (req: Request, res: Response) => {
+  // eslint-disable-next-line prefer-const
+  let errors: Array<statusCheck> = new Array<statusCheck>(),
+    errorCount = 0;
+  errors.push({ name: 'Database-Connection', error: DBError });
+  errors.push({
+    name: 'Register-Basic-Auth-Key',
+    error: process.env.REGISTER_BASIC_AUTH_KEY ? null : configErrors['HTTP-BASIC'],
+  });
+  errors.push({
+    name: 'Login-Basic-Auth-Key',
+    error: process.env.LOGIN_BASIC_AUTH_KEY ? null : configErrors['HTTP-BASIC'],
+  });
+  errors.push({
+    name: 'JWT-Token-Secret-Key',
+    error: process.env.JWT_TOKEN_SECRETKEY ? null : configErrors['JWT-SECRET'],
+  });
+  errors.forEach((errorObject, inx, array) => {
+    if (errorObject.error !== null) errorCount++;
+  });
+  res.status(errorCount === 0 ? 200 : 500).json(errors);
 });
 
 router.use('/auth/emailCheck', emailCheckAPI);
